3b9ef668cdb63822a865aae6ede1676136f4e077
grubertecnologia <grubertecnologia@gmail.com>
2024-01-28 16:10:46 -0300

    Primeiro commit

---
+++ buscar-cursos.php
@@ -0,0 +1,22 @@
+<?php
+
+require 'vendor/autoload.php';
+
+
+use GruberTecnologia\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client([
+    'base_uri' => 'https://www.alura.com.br/',
+    'verify' => false
+]);
+
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo exibeMensagem($curso);
+}
